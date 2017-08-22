4f0d79f4e262183b3da9cc43b83ae756d6089cfd
bduvenhage <bernardt.duvenhage@gmail.com>
2017-08-22 08:58:29 +0200

    Initial version of the server with date_parser, sentiment_detector and the essentials of the text_classifier model types.

--- python_wrapper/examples/date_parser.py
+++ python_wrapper/examples/date_parser.py
@@ -5,17 +5,18 @@ from feersum_nlu.rest import ApiException
 
 # Configure API key authorization: APIKeyHeader
 feersum_nlu.configuration.api_key['AUTH_TOKEN'] = 'YOUR_API_KEY'
-feersum_nlu.configuration.host = "http://127.0.0.1:8080/nlu/v2"
+feersum_nlu.configuration.host = "http://127.0.0.1:8000/nlu/v2"
 
 api_instance = feersum_nlu.DateParsersApi()
 
 model_instance_name = 'generic'
-text_input = feersum_nlu.TextInput("The day after tomorrow at 11:00 in the evening.") # TextInput | The input text.
+text_input = feersum_nlu.TextInput("The day after tomorrow at 11:00 in the evening.")  # TextInput | The input text.
 
 try:
     # Extract dates.
     api_response = api_instance.date_parser_retrieve(model_instance_name, text_input)
-    print(api_response)
+    print("type(api_response)", type(api_response))
+    print("api_response", api_response)
 except ApiException as e:
     print("Exception when calling DateParsersApi->date_parser_retrieve: %s\n" % e)
 except urllib3.exceptions.MaxRetryError:
--- python_wrapper/examples/sentiment_detector.py
+++ python_wrapper/examples/sentiment_detector.py
@@ -5,17 +5,18 @@ from feersum_nlu.rest import ApiException
 
 # Configure API key authorization: APIKeyHeader
 feersum_nlu.configuration.api_key['AUTH_TOKEN'] = 'YOUR_API_KEY'
-feersum_nlu.configuration.host = "http://127.0.0.1:8080/nlu/v2"
+feersum_nlu.configuration.host = "http://127.0.0.1:8000/nlu/v2"
 
 api_instance = feersum_nlu.SentimentDetectorsApi()
 
 model_instance_name = 'generic'
-text_input = feersum_nlu.TextInput("I am happy.") # TextInput | The input text.
+text_input = feersum_nlu.TextInput("I'm not happy.")  # TextInput | The input text.
 
 try:
     # Detect sentiment.
     api_response = api_instance.sentiment_detector_retrieve(model_instance_name, text_input)
-    print(api_response)
+    print("type(api_response)", type(api_response))
+    print("api_response", api_response)
 except ApiException as e:
     print("Exception when calling SentimentDetectorsApi->sentiment_detector_retrieve: %s\n" % e)
 except urllib3.exceptions.MaxRetryError:
