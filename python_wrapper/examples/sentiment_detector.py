e1a9d8b09317300573e0ee6c4bd8c8575b9f87eb
bduvenhage <bernardt.duvenhage@gmail.com>
2017-10-05 11:59:57 +0200

    Latest update of generated code and intro doc.

--- python_wrapper/examples/sentiment_detector.py
+++ python_wrapper/examples/sentiment_detector.py
@@ -4,10 +4,10 @@ import feersum_nlu
 from feersum_nlu.rest import ApiException
 
 # Configure API key authorization: APIKeyHeader
-feersum_nlu.configuration.api_key['AUTH_TOKEN'] = 'FEERSUM-NLU-f9b-bdc0-455e-a3b1-7c761'  # 'YOUR_API_KEY'
+feersum_nlu.configuration.api_key['AUTH_TOKEN'] = 'YOUR_API_KEY'
 
 # feersum_nlu.configuration.host = "http://127.0.0.1:8100/nlu/v2"
-feersum_nlu.configuration.host = "http://nlu.playground.feersum.io:8000/nlu/v2"
+feersum_nlu.configuration.host = "http://nlu.playground.feersum.io:8100/nlu/v2"
 
 api_instance = feersum_nlu.SentimentDetectorsApi()
 
