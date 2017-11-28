3bebebe7baa8ef4960d2907b11feffedefbb3a17
bduvenhage <bernardt.duvenhage@gmail.com>
2017-11-28 14:27:59 +0200

    Removed the jupyter dependency.

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ VERSION = "2.0.5"
 # prerequisite: setuptools
 # http://pypi.python.org/pypi/setuptools
 
-REQUIRES = ["urllib3 >= 1.15", "six >= 1.10", "certifi", "python-dateutil", "jupyter"]
+REQUIRES = ["urllib3 >= 1.15", "six >= 1.10", "certifi", "python-dateutil"]
 
 setup(
     name=NAME,
