58c5d2a00af947d28ddce64282bf43ca21774c67
bduvenhage <bernardt.duvenhage@gmail.com>
2019-06-24 23:39:57 +0200

    Bumped version for dev release.

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "feersum_nlu"
-VERSION = "2.0.33.dev2"
+VERSION = "2.0.33.dev3"
 
 # To install the library, run the following
 #
